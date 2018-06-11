b7f238cef89f387f38e708990eb86c3484c3a733
Robin Eriksson <robineriksson.mail@gmail.com>
2018-06-11 10:07:02 +0200

    placeholder for the functions to come

---
+++ R/createTrajectores.R
@@ -0,0 +1,26 @@
+library(SimInf)
+library(SimInfInference)
+
+##' dataGenerate
+##'
+##' Generate a trajectory
+##'
+##' Given parameter generate a trajectory.
+##' @param theta parameter vector
+##' @return data frame with time and value
+dataGenerate <- function(theta){}
+
+##' dataSave
+##'
+##' generate CSV file given list of trajectories
+##'
+##' @param data list of trajectories (data frames)
+##' @param filename name of the file
+##' @param dest where the file should be saved
+##' @return confirmations
+dataSave <- function(data, filename, dest){}
+
+##' main function
+##'
+##' Create 3 trajectories and save as csv file.
+main <- function(){}
