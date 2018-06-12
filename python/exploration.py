6a2f22f942defe07033095b1d1987b46aa276f0d
Robin Eriksson <robineriksson.mail@gmail.com>
2018-06-12 13:58:32 +0200

    extracting features. Not sure if correct. Need to evaluate

--- python/exploration.py
+++ python/exploration.py
@@ -2,7 +2,9 @@
 # import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from tsfresh import extract_features
+from tsfresh.utilities.dataframe_functions import impute
 
 
 # load the data
@@ -21,8 +23,67 @@ def plotData(data):
     return()
 
 
+# plot SS
+def plotSS(s0, s1, s2):
+    n = s0.shape[0]
+    plt.plot(range(0, n), s0)
+    plt.plot(range(0, n), s1)
+    plt.plot(range(0, n), s2)
+    plt.legend(["s0", "s1", "s2"])
+    plt.show()
+    return()
+
+
+# plot distance
+def plotdist(d1, d2):
+    n = d1.shape[0]
+    plt.plot(range(0, n), d1)
+    plt.plot(range(0, n), d2)
+    plt.legend(["d1", "d2"])
+    plt.show()
+    return()
+
+
+# clean SS
+def cleanSS(SS):
+    impute(SS)
+    s0 = np.array(SS.loc["obs.sample", :])
+    s1 = np.array(SS.loc["sim1.sample", :])
+    s2 = np.array(SS.loc["sim2.sample", :])
+    # remove zero elements
+    nonzero = np.isnan(s0/s0)
+    return(s0[~nonzero], s1[~nonzero], s2[~nonzero])
+
+
+# normalize SS
+def normalSS(s0, s1, s2):
+    n0 = s0/s0
+    n1 = s1/s0
+    n2 = s2/s0
+    return(n0, n1, n2)
+
+
+# eucl distance
+def distSS(n0, n1, n2):
+    d1 = np.sqrt(n0-n1)**2
+    d2 = np.sqrt(n0-n2)**2
+    return(d1, d2)
+
+
+# specify filename
 filename = "/home/rober323/Gits/SSexploring/DATA/genData.csv"
-dat = getData(filename)
+
+# load data
+fulldat = getData(filename)
+
+# melt the data frame
+dat = pd.melt(fulldat, id_vars="obs.time")
 
 SS = extract_features(dat,
-                      column_id="obs.sample")
+                      column_id="variable",
+                      column_value="value")
+s0, s1, s2 = cleanSS(SS)
+n0, n1, n2 = normalSS(s0, s1, s2)
+
+d1, d2 = distSS(n0, n1, n2)
+plotdist(d1, d2)
